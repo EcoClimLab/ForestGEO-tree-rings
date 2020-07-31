b12befb5abda140e6433cefefc6c996f70f0bb54
Valentine Herrmann <herrmannv@si.edu>
2020-07-31 10:47:37 -0400

    update figure for manuscript

--- scripts/Prepare_figures_for_manuscript.R
+++ scripts/Prepare_figures_for_manuscript.R
@@ -7,6 +7,7 @@ rm(list = ls())
 library(png)
 library(gridExtra)
 library(ggplot2)
+library(grid)
 
 # prepare site list and site coordinates ####
 sites <- list.dirs("results/log_core_measurement_dbh", full.names = F, recursive = F)
@@ -35,17 +36,20 @@ sites_coords$site <- names(sites.sitenames)[match(sites_coords$Site.name, sites.
 sites <- sites_coords$site[order(sites_coords$Latitude)]
 n_sites <- length(sites)
 
-# DBH response at each sites and for each response ####
-what_to_show <- c("log_core_measurement_dbh" = expression(Delta*r), "log_BAI_dbh" = expression(BAI), "log_agb_inc_dbh" = expression(Delta*AGB))
+# prepare function ####
 
-
-g_legend <- function(){
-  a.gplot <- ggplot(data = p$data, aes(x = varying_x, y = expfit)) + geom_line(aes(group = species, col = species)) +  geom_ribbon(aes(ymin=lwr, ymax=upr, col = NULL, bg = species), alpha=0.25) + theme(legend.title=element_blank())+ guides(col=guide_legend(ncol=2), bg=guide_legend(ncol=2))
+g_legend <- function(x = "pt"){
+  p <- get(x, temp_env)
+  a.gplot <- ggplot(data = p, aes(x = varying_x, y = expfit)) + geom_line(aes(group = species, col = species)) +  geom_ribbon(aes(ymin=lwr, ymax=upr, col = NULL, bg = species), alpha=0.25) + theme(legend.title=element_blank())+ guides(col=guide_legend(ncol=2), bg=guide_legend(ncol=2))
   tmp <- ggplot_gtable(ggplot_build(a.gplot))
   leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
   legend <- tmp$grobs[[leg]]
   return(legend)
-  }
+}
+# DBH response at each sites and for each response ####
+what_to_show <- c("log_core_measurement_dbh" = expression(Delta*r), "log_BAI_dbh" = expression(BAI), "log_agb_inc_dbh" = expression(Delta*AGB))
+
+
 
 all_plots <- list()
 for(site in sites){
@@ -58,11 +62,11 @@ for(site in sites){
     # remove title and xlab of p
     p$labels$title <- NULL
     p$labels$x <- NULL
-  
+    
     
     # save into all_plots
     all_plots[[paste0(site, what)]] <- p
-    } # for what in ...
+  } # for what in ...
   
   # add a plot for the legend
   all_plots[[paste0(site, "leg")]] <- g_legend()
@@ -75,61 +79,100 @@ grid.arrange(arrangeGrob(grobs = all_plots, ncol = 4, vp= grid::viewport(width=0
 
 grid::grid.text(sites, x = unit(0.01, "npc"), y = unit(rev(cumsum(c(1/n_sites/2, rep(1/n_sites, n_sites-1)))), "npc"), rot = 90)
 
-grid::grid.text(what_to_show, x = unit(cumsum(c(1/4/2, rep(1/4, 2))), "npc"), y = unit(.99,  "npc"))
+grid::grid.text(what_to_show, x = unit(cumsum(c(.05 +.9/4/2, rep(.9/4, 2))), "npc"), y = unit(.99,  "npc"))
 
 grid::grid.text("dbh (cm)", x = unit(.5, "npc"), y = unit(0.015,  "npc"))
 
 dev.off()
 
-# # preapre and save figures ####
-# for (site in sites) {
-#   imgs <- list.files(paste0("results/", c("log_core_measurement", "log_core_measurement_dbh", "log_agb_inc_dbh", "log_BAI_dbh"), "/", site), pattern = "climwin", full.names = T)
-#   
-#   variables <- regmatches(imgs, regexpr("climwin_\\D{3}_", imgs))
-#   variables <- unique(gsub("climwin|_", "", variables))
-#   
-#   for(v in variables) {
-#     
-#     imgs_v <- imgs[grepl(v, imgs)]                 
-#     nb_plots <- length(imgs_v)
-#     
-#     
-#     
-#     png(paste0("results/climwin_plots_combined/", site, "_",v, ".png" ), width = 10, height = 8, units = "in", res = 300)
-#     layout(matrix(1:(nb_plots*2), nrow = nb_plots, byrow = T), widths = c(4, 1))
-#     
-#     
-#     for(i in seq_along(imgs_v)){
-#       y_lab = gsub("log_|_inc", "", strsplit(imgs_v[i], "/")[[1]][2])
-#       img <- readPNG(imgs_v[i])
-#       img1 <- img[1:(nrow(img)/2), ,] # keep only first half of plot
-#       img2 <- img[c((nrow(img)/2)+1):nrow(img), c((ncol(img1)/4): (2*ncol(img1)/4)),] # keep only plot 2 of second row
-#       # img1[, (ncol(img1)+1):  (ncol(img1)+ncol(img1)/4), ] <- 0
-#       # 
-#       # img1[,c((ncol(img1)/4): (ncol(img1) - ncol(img1)/4)) , ] <- img1[,c( (2 *ncol(img1)/4): ncol(img1)) , ] #replace plot 2 and 3 by 3 and 4
-#       # img1[,c(  (ncol(img1) - ncol(img1)/4): ncol(img1)) , ] <- img[c((nrow(img)/2)+1):nrow(img), c((ncol(img1)/4): (2*ncol(img1)/4)),] # replace plot 4 by plot 6 in orginal image
-#       
-#       par(mar = c(0,3,0,0))
-#       plot(0:100, 0:100, type = "n", axes = F, xlab = "", ylab = "")
-#       
-#       rasterImage( img1 , xleft = 0, xright = 100,
-#                    ybottom = 0, ytop = 100)
-#       
-#       mtext(side = 2, text = y_lab)
-#       
-#       par(mar = c(0,0,0,0))
-#       plot(0:100, 0:100, type = "n", axes = F, xlab = "", ylab = "")
-#       
-#       rasterImage( img2 , xleft = 0, xright = 100,
-#                    ybottom = 0, ytop = 100)
-#       
-#     }
-#     
-#     dev.off()
-#     
-#     
-#     
-#     
-#   }
-#   
-# }
+# Pre, Temp and cld groups ####
+
+all_plots <- list()
+for(site in sites){
+  what = "log_core_measurement"
+  
+  temp_env <- new.env()
+  load(paste0('results/', what, "/", site, "/env.RData"), envir = temp_env) 
+  
+  existing_plots <- ls(temp_env)[grepl("^p_", ls(temp_env))]
+  
+  # get variable in order or Precipitation, Temperature and cloud groups (but complicated because, pet is in both temp and dtr),....
+  clim_var_group <- get("clim_var_group"  , temp_env)
+  
+  existing_plots <- existing_plots[match(c(1,2,3), sapply(gsub("p_", "", existing_plots), grep, clim_var_group))]
+  
+  assign("leg", g_legend(), envir = temp_env)
+  existing_plots <- c(existing_plots, "leg")
+  
+  all_plots[[paste0(site, what)]] <- grid.arrange(do.call(arrangeGrob, c(lapply(existing_plots, function(x)  {if(is.na(x)) grid.rect(gp=gpar(col="white")) else get(x, temp_env)}), ncol = 4)))
+  
+} # for what in ...
+
+png("doc/manuscript/tables_figures/pre_temp_cld_groups.png", width = 8, height = 10, res = 300, units = "in")
+
+grid.arrange(grobs = all_plots, vp= grid::viewport(width=0.95, height=0.95), ncol = 1)
+
+
+grid::grid.text(sites, x = unit(0.01, "npc"), y = unit(rev(cumsum(c(1/n_sites/2, rep(1/n_sites, n_sites-1)))), "npc"), rot = 90)
+
+grid::grid.text(c("Precipiation group", "Temperature group", "Cloud group"), x = unit(cumsum(c(.05 +.9/4/2, rep(.9/4, 2))), "npc"), y = unit(.99,  "npc"))
+
+
+dev.off()
+
+# comparison with quilt ####
+site = "SCBI"
+v = "pre"
+what = "log_core_measurement"
+
+png("doc/manuscript/tables_figures/quilt_comparison.png", width = 10, height = 8 , units = "in", res = 300)
+
+layout(matrix(c(1, 1, 2, 3, 5, 5, 4, 6, 6, 7, 7, 7), nrow = 3), heights = c(2,1,1), widths = c(2, 1, 1, 2))
+par(mar = c(0,0,0,0))
+
+## a) quilt ####
+img <- readPNG(paste0("results/traditional_analysis/", site, "/1901_2009/figures/monthly_correlation/", v, ".png"))
+img1 <- img[50:(nrow(img)-150), 1:825,] # keep only quilt half of plot
+img2 <- img[,825:(ncol(img)-50), ] # keep only legend
+
+
+par(mar = c(0,0,0,0))
+plot(0:100, 0:100, type = "n", axes = F, xlab = "", ylab = "")
+
+rasterImage( img1 , xleft = 0, xright = 100,
+             ybottom = 0, ytop = 100)
+
+par(mar = c(0,0,0,0))
+plot(0:100, 0:100, type = "n", axes = F, xlab = "", ylab = "")
+
+rasterImage( matrix(as.vector(as.raster(img2)), ncol = nrow(img2))[ncol(img2):1,], xleft = 0, xright = 100,
+             ybottom = 0, ytop = 100)
+
+## b,c,d,e) climwin ####
+img <- readPNG(paste0("results/", what, "/", site, "/climwin_", v, "_quad_14_0_SCBI.png"))
+
+img1 <- img[80:(nrow(img)/2), c((2*ncol(img)/4): (3*ncol(img)/4)) ,] # beta linear
+img2 <- img[80:(nrow(img)/2), c((3*ncol(img)/4): (4*ncol(img)/4)) ,] # beta quadratic
+img3 <- img[80:(nrow(img)/2), c(1: (1*ncol(img)/4)) ,] # AIC
+img4 <- img[c((nrow(img)/2)+1):nrow(img), c((1*ncol(img)/4): (2*ncol(img)/4)) ,] # response
+
+for(i in 1:4) {
+  plot(0:100, 0:100, type = "n", axes = F, xlab = "", ylab = "")
+  
+  rasterImage( get(paste0("img", i)) , xleft = 0, xright = 100,
+               ybottom = 0, ytop = 100)
+}
+
+## f) response curves ####
+temp_env <- new.env()
+load(paste0("results/", what, "/", site, "/env.RData"), temp_env)
+
+p <- get(paste0("p_", v), temp_env)
+plot.new()              ## suggested by @Josh
+vps <- gridBase::baseViewports()
+pushViewport(vps$figure) ##   I am in the space of the autocorrelation plot
+vp1 <- plotViewport(c(1.8,1,0,1)) ## create new vp with margins, you play with this values 
+
+print(p,vp = vp1) 
+## dev.off() ####
+dev.off()
