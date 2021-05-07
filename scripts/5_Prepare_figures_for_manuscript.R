1564070fecf9719947918927da824f65ff0526bb
Valentine Herrmann <herrmannv@si.edu>
2021-05-07 16:30:43 -0400

    standardize y-axis

--- scripts/5_Prepare_figures_for_manuscript.R
+++ scripts/5_Prepare_figures_for_manuscript.R
@@ -321,11 +321,17 @@ for(what in names(what_to_show)) {
       # sandardize variable names
       lapply(existing_plots[!is.na(existing_plots)], function(x) {
         p <- get(x, temp_env) 
+        
+        ylim_p_int <- get("ylim_p_int", temp_env)
+        ylim_p_int <- ylim_p_int[!names(ylim_p_int) %in% "dbh"]
+   
+        p <- p + ylim(range(ylim_p_int))
         p$labels$x <- eval(parse(text = gsub(" |  ", "~", gsub("-1", "\\^-1", paste0(gsub(substr(p$labels$x, 1, 4), v_names[substr(p$labels$x, 1, 3)], p$labels$x), ")")))))
         p$theme$plot.background <- element_blank()
         p$theme$legend.position <- "none"
         
         assign(x, p, temp_env)
+        print(p)
       })
     }
     
@@ -383,6 +389,12 @@ for(site in names(sites_species_to_show)){
     # sandardize variable names
     lapply(existing_plots[!is.na(existing_plots)], function(x) {
       p <- get(x, temp_env) 
+      
+      ylim_p_int <- get("ylim_p_int", temp_env)
+      ylim_p_int <- ylim_p_int[!names(ylim_p_int) %in% "dbh"]
+      
+      p <- p + ylim(range(ylim_p_int))
+      
       p$labels$x <- eval(parse(text = gsub(" |  ", "~", gsub("-1", "\\^-1", paste0(gsub(substr(p$labels$x, 1, 4), v_names[substr(p$labels$x, 1, 3)], p$labels$x), ")")))))
       p$theme$plot.background <- element_blank()
       
